30a1376da4dc48368242d04ac789404c292c877f
David Nichols <david@qore.org>
2025-01-24 19:44:25 +0100

    increased stack guard on macOS aarch64

--- include/qore/macros-aarch64.h
+++ include/qore/macros-aarch64.h
@@ -5,9 +5,14 @@
 
 #define STACK_DIRECTION_DOWN 1
 
+#if defined(__APPLE__)
+// empirical testing on macOS 15.2 shows that 80K is sufficient to catch stack exhaustion errors before a crash
+#define QORE_STACK_GUARD (80 * 1024)
+#else
 // tests fail with a stack guard smaller than 34K in Docker instances on ARM Graviton machines
 // (CI cloud test environment) - tests pass on the same HW not in Docker with a much smaller value
 #define QORE_STACK_GUARD (34 * 1024)
+#endif
 
 #ifdef __GNUC__
 
