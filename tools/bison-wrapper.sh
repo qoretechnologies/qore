816db3e8fcd57d512912aa414592b044329236d3
David Nichols <david@qore.org>
2026-01-19 19:22:33 +0100

    build: add bison wrapper and warning suppression

---
+++ tools/bison-wrapper.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+set -e
+
+bison_real="${BISON_REAL:-bison}"
+
+if [ -z "$bison_real" ]; then
+    bison_real="bison"
+fi
+
+for arg in "$@"; do
+    case "$arg" in
+        --version|-V|--help|-h)
+            exec "$bison_real" "$@"
+            ;;
+    esac
+done
+
+input=""
+for arg in "$@"; do
+    case "$arg" in
+        -*)
+            ;;
+        *)
+            if [ -f "$arg" ]; then
+                input="$arg"
+            fi
+            ;;
+    esac
+done
+
+if [ -z "$input" ]; then
+    exec "$bison_real" "$@"
+fi
+
+version=$("$bison_real" --version | head -n 1 | awk '{print $NF}')
+major=$(printf '%s' "$version" | awk -F. '{print $1}')
+minor=$(printf '%s' "$version" | awk -F. '{print $2}')
+
+if [ -z "$major" ] || [ -z "$minor" ]; then
+    exec "$bison_real" "$@"
+fi
+
+use_new_directives=0
+if [ "$major" -gt 3 ] || { [ "$major" -eq 3 ] && [ "$minor" -ge 0 ]; }; then
+    use_new_directives=1
+fi
+
+if [ "$use_new_directives" -eq 0 ]; then
+    exec "$bison_real" "$@"
+fi
+
+tmp="$(mktemp)"
+trap 'rm -f "$tmp"' EXIT
+
+sed -e 's/^[[:space:]]*%pure-parser[[:space:]]*$/%define api.pure full/' \
+    -e 's/^[[:space:]]*%error-verbose[[:space:]]*$/%define parse.error verbose/' \
+    "$input" > "$tmp"
+
+set -- "$@"
+new_args=""
+for arg in "$@"; do
+    if [ "$arg" = "$input" ]; then
+        new_args="$new_args \"$tmp\""
+    else
+        new_args="$new_args \"$arg\""
+    fi
+done
+
+eval exec "\"$bison_real\"" "$new_args"
